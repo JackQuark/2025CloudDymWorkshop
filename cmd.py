c273ea43ecf86e519d0d04f1254c62790834d055
Quark <yen169054@gmail.com>
2025-04-05 17:30:52 +0800

    commit at 2025-04-05 17:30:52.306084

--- cmd.py
+++ cmd.py
@@ -1,7 +1,11 @@
 # auto push
 
+import os
 import subprocess
 import datetime
+
+os.system("copy \Campbellsci\LoggerNet\CR1000_2_Data1min.dat .")
+os.system("Yes")
 
 subprocess.call(["git", "add", "."])
 subprocess.call(["git", "commit", "-m", "commit at " + str(datetime.datetime.now())])
