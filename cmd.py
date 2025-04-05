25e775bb8d3ec29858fdf916a4cc75a36b50c076
Quark <yen169054@gmail.com>
2025-04-05 20:34:06 +0800

    commit at 2025-04-05 20:34:06.148955

--- cmd.py
+++ cmd.py
@@ -5,7 +5,6 @@ import subprocess
 import datetime
 
 os.system("copy \Campbellsci\LoggerNet\CR1000_2_Data1min.dat .")
-os.system("Yes")
 
 subprocess.call(["git", "add", "."])
 subprocess.call(["git", "commit", "-m", "commit at " + str(datetime.datetime.now())])
